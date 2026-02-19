917f0b3bd81b80597c8c8cce0dd2ec9784236c1f
onlinebestsupport <info@bestsupportonline.in>
2026-02-20 00:48:47 +0530

    Delete app/routes/server.py

--- app/routes/server.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from app import app
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
