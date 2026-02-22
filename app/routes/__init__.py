22a44643a0906f3559c7cbe2d0edbe845d3253a9
onlinebestsupport <info@bestsupportonline.in>
2026-02-22 23:17:42 +0530

    Create __init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,8 @@
+# This file makes the routes directory a Python package
+from . import auth
+from . import admin
+from . import batches
+from . import travelers
+from . import payments
+from . import company
+from . import uploads
